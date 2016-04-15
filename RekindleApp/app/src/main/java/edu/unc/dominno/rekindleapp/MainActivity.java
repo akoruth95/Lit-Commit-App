f9cd6a242ff2079945f61d4bb101a505aa24dac2
Victor Cui <victor.cui1996@gmail.com>
2016-04-15 16:26:58 -0400

    Changed FB UI to only show FB login on MainActivity

--- RekindleApp/app/src/main/java/edu/unc/dominno/rekindleapp/MainActivity.java
+++ RekindleApp/app/src/main/java/edu/unc/dominno/rekindleapp/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.unc.dominno.rekindleapp;
 
+import android.app.TabActivity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 import android.widget.TextView;
 // Add this to the header of your file:
 import com.facebook.AccessToken;
@@ -15,9 +18,11 @@ import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+
 //implements FacebookCallback
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tabHost;
     private TextView fbLoginInfo;
     private LoginButton loginButton; //FB Login button
     private CallbackManager callbackManager; //manages callbacks used in FB
@@ -30,18 +35,45 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
+        setContentView(R.layout.activity_main);
+
+        /* tabHost = (TabHost)findViewById(android.R.id.tabhost);
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("Second Tab");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("Third Tab");
+        TabHost.TabSpec tab4 = tabHost.newTabSpec("Fourth Tab");
+
+        tab1.setIndicator("Home");
+        Intent i = new Intent(this, Ratings.class);
+        tab1.setContent(i);
+        startActivity(i);
+
+        tab2.setIndicator("My Ratings");
+        tab2.setContent(new Intent(this, ShowRatings.class));
+
+        tab3.setIndicator("", getResources().getDrawable(R.drawable.micon));
+        tab3.setContent(new Intent(this, Messages.class));
+
+        tab4.setIndicator("FAQ");
+        tab4.setContent(new Intent(this, FAQ.class));
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+        tabHost.addTab(tab4); */
 
         //FB Login Integration
         callbackManager = CallbackManager.Factory.create();
-        /* accessTokenTracker = new AccessTokenTracker() {
+        accessTokenTracker = new AccessTokenTracker() {
             @Override
-            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
-                //set the access token using currentAccessToken when it's loaded or set
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken newAccessToken) {
+                //will be called when there's a change in the Current Access Token (will only help if user's already logged in )
+                //updateWithToken(newAccessToken);
 
             }
-            //If the access token is available already assign it
 
-        }; */
+        };
         fbLoginInfo = (TextView) findViewById(R.id.fbInfo);
         loginButton = (LoginButton) findViewById(R.id.fb_login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -67,7 +99,23 @@ public class MainActivity extends AppCompatActivity {
                 fbLoginInfo.setText("Login attempt failed: " + error.getMessage());
             }
         });
+        //Used to check if a user is currently logged in to FB
+        //updateWithToken(AccessToken.getCurrentAccessToken());
 
+    }
+
+    /* private void updateWithToken(AccessToken currentAccessToken) {
+        if (isLoggedIn()) {
+            Intent i = new Intent()
+        } else {
+
+        }
+    } */
+
+    //checks if a user is currently logged in to FB
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 
     @Override
